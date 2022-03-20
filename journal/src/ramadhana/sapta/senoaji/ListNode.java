f53394cc7036d5e3d81f9782fc40046bb7e51302
Revon Zev <senoaji2002@gmail.com>
2022-03-20 10:48:06 +0700

    Added "perv" Variable

--- journal/src/ramadhana/sapta/senoaji/ListNode.java
+++ journal/src/ramadhana/sapta/senoaji/ListNode.java
@@ -3,10 +3,12 @@ package ramadhana.sapta.senoaji;
 public class ListNode<E> {
     E data;
     ListNode<E> next;
+    ListNode<E> perv;
 
     public ListNode(E object){
         data = object;
         this.next = null;
+        this.perv = null;
     }
 
     public E getData(){
