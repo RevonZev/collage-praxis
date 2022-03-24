6b8e80eab1d8ff5c9881b1ad8376ef6cbf9a3827
Revon Zev <senoaji2002@gmail.com>
2022-03-24 11:29:01 +0700

    Infix to suffix

---
+++ journal/src/ramadhana/sapta/senoaji/InfixToPostFixEvaluation.java
@@ -0,0 +1,74 @@
+package ramadhana.sapta.senoaji;
+
+import java.util.Stack;
+
+// sam_k, Infix to Postfix using Stacks Java,
+// https://stackoverflow.com/questions/26699089/infix-to-postfix-using-stacks-java,
+// 24/03/2022; edited at line 32 to add numbers, not just letters
+public class InfixToPostFixEvaluation {
+
+    private static boolean isOperator(char c) {
+        return c == '+' || c == '-' || c == '*' || c == '/' || c == '^' || c == '(' || c == ')';
+    }
+
+    private static int getPrecedence(char ch) {
+        switch (ch) {
+            case '+':
+            case '-':
+                return 1;
+
+            case '*':
+            case '/':
+                return 2;
+
+            case '^':
+                return 3;
+        }
+        return -1;
+    }
+
+    // A utility function to check if the given character is operand
+    private static boolean isOperand(char ch) {
+        return (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') || (ch >= '0' && ch <= '9');
+    }
+
+    public static String convertToPostfix(String infix) {
+        Stack<Character> stack = new Stack<Character>();
+        StringBuffer postfix = new StringBuffer(infix.length());
+        char c;
+
+        for (int i = 0; i < infix.length(); i++) {
+            c = infix.charAt(i);
+
+            if (isOperand(c)) {
+                postfix.append(c);
+            } else if (c == '(') {
+                stack.push(c);
+            }
+            // If the scanned character is an ‘)’, pop and output from the stack
+            // until an ‘(‘ is encountered.
+            else if (c == ')') {
+
+                while (!stack.isEmpty() && stack.peek() != '(') {
+                    postfix.append(stack.pop());
+                }
+                if (!stack.isEmpty() && stack.peek() != '(')
+                    return null;
+                else if(!stack.isEmpty())
+                    stack.pop();
+            }
+            else if (isOperator(c)) // operator encountered
+            {
+                if (!stack.isEmpty() && getPrecedence(c) <= getPrecedence(stack.peek())) {
+                    postfix.append(stack.pop());
+                }
+                stack.push(c);
+            }
+        }
+
+        while (!stack.isEmpty()) {
+            postfix.append(stack.pop());
+        }
+        return postfix.toString();
+    }
+}
--- journal/src/ramadhana/sapta/senoaji/Main.java
+++ journal/src/ramadhana/sapta/senoaji/Main.java
@@ -2,71 +2,14 @@ package ramadhana.sapta.senoaji;
 
 import java.util.Scanner;
 
+import static ramadhana.sapta.senoaji.InfixToPostFixEvaluation.convertToPostfix;
+
 public class Main {
-//      Store:
-//      ✔ Judul Lagu
-//      ✔ Nama Penyani
-//
-//      ✔ Dianggap diputar saat didisplaykan semua
-//
-//      Ada:
-//      ✔ Input lagu (di akhir)
-//      ✔ Hapus lagu (✔ terakhir masuk & ✔ sesuai judul)
-//      ✔ Memainkan lagu (display semua, awal > akhir > awal)
     static Scanner sc = new Scanner(System.in);
-    static DoublyLinkedList<Lagu> linkedList = new DoublyLinkedList<>();
 
     public static void main(String[] args) {
-        while (true) {
-            switch (mainMenu()) {
-                case 1:
-                    inputMenu();
-                    break;
-                case 2:
-                    eraseMenu();
-                    break;
-                case 3:
-                    displayMenu();
-                    break;
-                default:
-                    System.exit(0);
-            }
-        }
-    }
-
-    private static void displayMenu() {
-        linkedList.print();
-    }
-
-    private static void eraseMenu() {
-
-        System.out.print("1. Hapus lagu di akhir list\n2. Hapus lagu dengan judul\nMasukkan angka: ");
-        int usrIn = sc.nextInt();
-        if (usrIn == 1) {
-            linkedList.removeFromBack();
-        } else {
-            System.out.print("Judul: ");
-            String judul = sc.nextLine(); sc.next();
-            linkedList.removeByJudul(judul);
-        }
-        System.out.println();
-    }
-
-    private static void inputMenu() {
-        System.out.print("Judul: ");
-        String judul = sc.next();
-        System.out.print("Penyanyi: ");
-        String penyanyi = sc.next();
-
-        linkedList.insertAtBack(new Lagu(judul, penyanyi));
-
-        System.out.println();
-    }
-
-    private static int mainMenu() {
-        System.out.print("1. Input lagu\n2. Hapus lagu\n3. Mainkan lagu\n4. Keluar\nMasukkan angka: ");
-        int usrIn = sc.nextInt();
-        System.out.println();
-        return usrIn;
+        // ✔ Infix to postfix
+        System.out.print("Infix to postfix: ");
+        System.out.println(convertToPostfix(sc.nextLine()));
     }
 }
