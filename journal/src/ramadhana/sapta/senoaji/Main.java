071c31991063e058747292a80b7b761bd6d00b0a
Revon Zev <senoaji2002@gmail.com>
2022-03-30 08:27:59 +0700

    Linear search

--- journal/src/ramadhana/sapta/senoaji/Main.java
+++ journal/src/ramadhana/sapta/senoaji/Main.java
@@ -1,11 +1,18 @@
 package ramadhana.sapta.senoaji;
 
-import java.util.Scanner;
+import java.util.LinkedList;
+import java.util.List;
+import ramadhana.sapta.senoaji.Search;
 
 public class Main {
-//    static Scanner sc = new Scanner(System.in);
-
     public static void main(String[] args) {
+        int result;
+
+        // Soal Linear Search
+        LinkedList<Integer> soalLinearSearch = new LinkedList<Integer>();
+        soalLinearSearch.addAll(List.of(new Integer[]{30, 4, 7, 10, 8, 100, 33}));
+        result = Search.linear(9, soalLinearSearch);
+        System.out.println((result == -1) ? "Not Found" : "Found" );
     }
 
 }
